f7fe9d1d814759fef88c440b661dd03d5859e866
salehi <h.salehi@metanext.biz>
2024-08-01 15:02:52 +0330

    finish the simple resolver

--- src/Http/Controllers/QLController.php
+++ src/Http/Controllers/QLController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace LaravelQL\LaravelQL\Http\Controllers;
+use GraphQL\GraphQL;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Schema;
+use GraphQL\Type\SchemaConfig;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
@@ -14,18 +16,32 @@ class QLController extends Controller
         $queryType = new ObjectType([
             'name' => 'Query',
             'fields' => [
-                'hello' => Type::string(),
-                'resolve' => fn($rootValue, array $args): string => 'Hello World'
+                'hello' => [
+                    'type' => Type::string(),
+                    'resolve' => function($rootVal , $args): string{
+                        Log::info("HELLO IN RESOLVER");
+
+                        return "HELLOS";
+                    }
+                ],
             ]
         ]);
-        $schema = new Schema([
-            'query' => $queryType
-        ]);
+        $schema = new Schema(
+            (new SchemaConfig())->setQuery($queryType)
+        );
 
         $query = $request->input('query');
-        return response()->json([
-            'message' => config('logging.channels.single.path'),
-            'path' => dirname(__DIR__)
-        ]);
+        try {
+            $result = GraphQL::executeQuery($schema , $query , [] , null , []);
+            Log::info("",[$result]);
+            $output = $result->toArray();
+        }catch (\Exception $e){
+            $output = [
+              'errors' => [
+                  'message' => $e->getMessage()
+              ]
+            ];
+        }
+        return response()->json($output);
     }
 }
